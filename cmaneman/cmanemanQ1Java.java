3f6f883055201954f719a3f39bdcacf68e5c89db
Christopher E <48299779+cmaneman@users.noreply.github.com>
2021-06-02 23:50:50 -0400

    cmanemanQ1Java
    -This is the Java version of Question 1

---
+++ cmaneman/cmanemanQ1Java.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class cmanemanQ1Java
+{
+    
+    public static int fib(int n1, int n2, int n)
+    {
+        int sum = 0;
+        
+        for(int i = 0; i<n; i++)
+        {
+            sum = n1 + n2;
+            
+            n1 = n2;
+            n2 = sum;
+        }
+        
+        return sum;
+    }
+    
+    public static void main(String[] args)
+    {
+        int num1 = 0, num2 = 0, n = 0;
+        
+        System.out.println("This program will invoke the Fibonacci sequence in Java\n");
+        System.out.print("How many times would like the program to perform the sequence: ");
+        Scanner s1 = new Scanner(System.in);
+        
+        n = s1.nextInt();
+        
+        System.out.print("You chose: " + n + " times");
+        
+        fib(num1, num2, n);
+    }
+}
+
+
