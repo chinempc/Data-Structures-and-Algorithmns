7022a23ab30db97f4fc384d889a3f19c5cc6fb35
Christopher E <48299779+cmaneman@users.noreply.github.com>
2021-06-08 21:35:51 -0400

    Upload of Question 2
    -Uses dynamic programming
    -Uses recursion
    -Uses Fibonacci

---
+++ cmaneman/cmanemanQ2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+//This program will use recursion and dynamic programming...
+
+void DynFib(int[], int, int, int);
+
+int main()
+{
+	const int ARRSIZE = 12;
+	int numArr[ARRSIZE] = {0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+	int i = 1;
+	int sum = 0;
+
+	DynFib(numArr, ARRSIZE, i, sum);
+
+	return 0;
+}
+
+void DynFib(int Narr[], int size, int i, int sum)
+{
+	if (i == 0)
+	{
+		sum = 0;
+	}
+	
+	if (i < 12)
+	{
+		
+		sum = Narr[i - 1] + Narr[i];
+
+		cout << "Iteration " << i << ": " << Narr[i - 1] << " + " << Narr[i] << " = " << sum << endl;
+
+		Narr[i + 1] = sum;
+		i++;
+
+		DynFib(Narr, size, i, sum);
+	}
+}
